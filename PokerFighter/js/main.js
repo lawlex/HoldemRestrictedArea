ec80331e7898cb2e30bd5a9b4b49dd386f1b2f17
Alexey Kuznetsov <alex.kuznecov@gmail.com>
2020-02-14 20:47:59 +0300

    update main.js

--- PokerFighter/js/main.js
+++ PokerFighter/js/main.js
@@ -2,3 +2,38 @@ $(document).ready(function(){
   /* this script works */
   /* you can type your code here */
 });
+/*
+
+$('<a>',{
+  text: 'Я конетейнер-ссылка',
+  href: 'http://google.com',
+  target: "_blank",
+  css: {
+    color: 'green',
+    backgroundColor: 'black',
+    display: 'block',
+    position: 'relative',
+    padding: '10px',
+  },
+  width: 200,
+  height: 100,
+  offset: {
+    top: 20,
+    left: 120,
+  },
+  on: {
+    click: function(event){
+      alert('click');
+    },
+    mouseover: function(event){
+      alert('mouse');
+      $(this).off('mouseover');
+    }
+  },
+  append: $('<br>')
+    .add($('<span>',{
+      text: 'text',
+      css:{fonweight: 'bold'}
+    })),
+}).appendTo('#wrapper');
+*/
